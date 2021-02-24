6a02729bbd2f76f2df93886d6768f5488f454878
janisl-dev <locmelis.j@gmail.com>
2021-02-24 19:53:08 +0200

    CheckPassFail done

--- src/com/rcs/classwork/Day5/CheckPassFail.java
+++ src/com/rcs/classwork/Day5/CheckPassFail.java
@@ -1,8 +1,18 @@
 package com.rcs.classwork.Day5;
 
+import java.util.Scanner;
+
 public class CheckPassFail {
 
     public static void main(String[] args) {
-	    // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ievadi skaitli no 1 līdz 100 ");
+        int num = scanner.nextInt();
+            if (num >= 50) {
+                System.out.println("PASS");
+            }else {
+                    System.out.println("FAIL");
+                }
+            System.out.println("DONE");
     }
 }
